f82a4486e2bd50be08c30b600076a778ad6cd114
AnandhuSubash <45092230+AnandhuSubash@users.noreply.github.com>
2019-02-02 16:57:08 +0530

    30-01-19

---
+++ MvcDatabase/MvcDatabase/Models/UserContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace MvcDatabase.Models
+{
+    public class UserContext:DbContext
+    {
+        public UserContext()
+            : base("mycon")
+        {
+            DropCreateDatabaseIfModelChanges<UserContext> d = new DropCreateDatabaseIfModelChanges<UserContext>();
+            Database.SetInitializer<UserContext>(d);
+        }
+
+        public DbSet<user> users { get; set; }
+    }
+}
---
+++ MvcDatabase/MvcDatabase/Models/user.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcDatabase.Models
+{
+    public class user
+    {
+        public int Id{get;set;}
+
+        [Required(ErrorMessage="Enter name"),MaxLength(30)]
+        [DataType(DataType.Text)]
+        [Display(Name="Student Name")]
+        public string name
+        {
+            get;
+            set;
+        }
+        [Required(ErrorMessage="Enter username"),MaxLength(30)]
+        [DataType(DataType.Text)]
+        [Display(Name="User Name")]
+        public string username
+        {
+            get;
+            set;
+        }
+        [Required(ErrorMessage="Enter Password"),MaxLength(30)]
+        [DataType(DataType.Text)]
+        public string password
+        {
+        get;
+        set;
+         }
+    }
+}
