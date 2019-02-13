79b21cbb4e9c975085e9c42e97c2240645501f24
AnandhuSubash <45092230+AnandhuSubash@users.noreply.github.com>
2019-02-13 17:09:01 +0530

    12-02-19
    laravel

---
+++ project1/app/Ajce.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Ajce extends Model
+{
+    //
+}
---
+++ project1/app/Http/Controllers/McaController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\mca;
+use Illuminate\Http\Request;
+
+class McaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\mca  $mca
+     * @return \Illuminate\Http\Response
+     */
+    public function show(mca $mca)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\mca  $mca
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(mca $mca)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\mca  $mca
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, mca $mca)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\mca  $mca
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(mca $mca)
+    {
+        //
+    }
+}
